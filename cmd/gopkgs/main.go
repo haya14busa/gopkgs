6a37b6f01d29d32b52500dbc6ee61ed6385e4e02
haya14busa <hayabusa1419@gmail.com>
2016-09-06 20:35:32 +0900

    introduce gopkg command

---
+++ cmd/gopkgs/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/haya14busa/gopkgs/x/tools/imports"
+)
+
+var (
+	fullpath = flag.Bool("fullpath", false, `output absolute file path to package directory. ("/usr/lib/go/src/net/http")`)
+	short    = flag.Bool("short", false, `output vendorless import path ("net/http", "foo/bar/vendor/a/b")`)
+)
+
+func main() {
+	flag.Parse()
+
+	if len(flag.Args()) > 0 {
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+
+	for _, pkg := range imports.GoPath() {
+		out := pkg.ImportPath
+		if *fullpath {
+			out = pkg.Dir
+		} else if *short {
+			out = pkg.ImportPathShort
+		}
+		fmt.Println(out)
+	}
+}
