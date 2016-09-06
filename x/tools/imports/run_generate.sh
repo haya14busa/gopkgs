5261e7171a3950d617c17360cefe818ee55f8620
haya14busa <hayabusa1419@gmail.com>
2016-09-06 18:43:32 +0900

    x/tools/imports LICENSE

--- x/tools/imports/run_generate.sh
+++ x/tools/imports/run_generate.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 ls $GOPATH/src/golang.org/x/tools/imports/*.go | grep --invert-match -e '_test.go$' | grep -e 'fastwalk*' -e 'fix.go' -e 'zstdlib.go' | xargs -i cp {} ./
+cp $GOPATH/src/golang.org/x/tools/LICENSE ./
