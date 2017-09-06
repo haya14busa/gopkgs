96eb789e693852a473fa51309ebed21b92f60d74
Nuruddin Ashr <uudashr@gmail.com>
2017-09-06 14:03:24 +0700

    Use -f to get output using specific format

--- cmd/gopkgs/main.go
+++ cmd/gopkgs/main.go
@@ -4,21 +4,50 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"html/template"
 	"os"
+	"text/tabwriter"
 
 	"github.com/haya14busa/gopkgs/x/tools/imports"
 )
 
 var (
-	fullpath = flag.Bool("fullpath", false, `output absolute file path to package directory. ("/usr/lib/go/src/net/http")`)
-	short    = flag.Bool("short", true, `output vendorless import path ("net/http", "foo/bar/vendor/a/b")`)
+	format = flag.String("f", "{{.ImportPath}}", "output format of the package")
 )
+
+var usageInfo = `
+Use -f to custom the output using template syntax. The struct being passed to 
+template is:
+	type Pkg struct {
+		Dir             string // absolute file path to Pkg directory ("/usr/lib/go/src/net/http")
+		ImportPath      string // full Pkg import path ("net/http", "foo/bar/vendor/a/b")
+		ImportPathShort string // vendorless import path ("net/http", "a/b")
+	}
+`
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+	flag.PrintDefaults()
+	fmt.Fprintln(os.Stderr)
+	tw := tabwriter.NewWriter(os.Stderr, 0, 0, 4, ' ', tabwriter.AlignRight)
+	fmt.Fprintln(tw, usageInfo)
+}
+
+func init() {
+	flag.Usage = usage
+}
 
 func main() {
 	flag.Parse()
 
 	if len(flag.Args()) > 0 {
-		flag.PrintDefaults()
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	tpl, err := template.New("out").Parse(*format)
+	if err != nil {
+		fmt.Fprintln(os.Stderr)
 		os.Exit(2)
 	}
 
@@ -26,12 +55,7 @@ func main() {
 	defer w.Flush()
 
 	for _, pkg := range imports.GoPath() {
-		out := pkg.ImportPath
-		if *fullpath {
-			out = pkg.Dir
-		} else if *short {
-			out = pkg.ImportPathShort
-		}
-		fmt.Fprintln(w, out)
+		tpl.Execute(w, pkg)
+		fmt.Fprintln(w)
 	}
 }
